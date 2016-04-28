4b8404b89f599863ffff85bf5fd184c7c0fde33f
leydson-vieira <leydson.vieira@gmail.com>
2016-04-28 15:00:44 -0300

    Creating new file mountbackup.py

---
+++ mountbackup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+import logging
+import os
+
+now = datetime.now()
+
+logging.basicConfig(filename='/var/log/mountbackup.log',level=logging.DEBUG)
+
+os.chdir('/')
+
+if os.path.exists("/bkp/dados"):
+
+  os.system('umount /bkp')
+  logging.info(' %s/%s/%s - %s:%s:%s backup desmontado' % (now.day, now.month, now.year, now.hour, now.minute, now.second))
+
+else:
+
+  os.system('mount -a')
+  logging.info(' %s/%s/%s - %s:%s:%s backup montado' % (now.day, now.month, now.year, now.hour, now.minute, now.second))
+
+# ------- FIM -------
