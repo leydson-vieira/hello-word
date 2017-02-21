6c0a4e0ca336268c3813faf216e09add4932a2ce
leydson-vieira <leydson.vieira@gmail.com>
2017-02-21 13:09:56 -0300

    Atualizando script de backup

--- backup_redmine.sh
+++ backup_redmine.sh
@@ -3,33 +3,33 @@
 echo "$(date +%d-%m-%y) $(date +%H:%M-%S) - Backup iniciado" >> /var/log/backup.log
 echo "Copiando base de dados..."
 
-mysqldump --user=root --password=admin43211 --skip-extended-insert redmine > /home/infra/testebkp.sql
+mysqldump --user=root --password=<password> --skip-extended-insert <database> > /home/<directory>/<name_backup>.sql
 
 echo "Base de dados - OK"
 echo
 echo "Copiando diretório de arquivos..."
 
-cp -r /opt/redmine/redmine-3.2.3/files /home/infra/
+cp -r /opt/redmine/redmine-3.2.3/files /home/<directory>/
 
 echo "Arquivos - OK"
 echo 
-cd /home/infra/
+cd /home/<directory>/
 
 echo "Compactando backup..."
 
-tar -czf /backup/redmine-$(date +%d-%m-%y).tar.gz files testebkp.sql
+tar -czf /backup/redmine-$(date +%d-%m-%y).tar.gz files <name_backup>.sql
 
 echo "Backup compactado - OK"
 echo
 echo "Removendo arquivos temporários..."
 
-rm -r files/ testebkp.sql
+rm -r files/ <name_backup>.sql
 
 echo "Remoção de arquivos - OK"
 echo
 echo "Backup Finalizado!"
 echo
 
-find /backup -name *.tar.gz -mtime +5 -exec rm {} ';'
+find /backup -name *.tar.gz -mtime +5 -exec rm {} ';' #exclui backups de 5 dias atrás
 
 echo -e "$(date +%d-%m-%y) $(date +%H-%M-%S) - Backup finalizado\n" >> /var/log/backup.log
