44fa6ae72051c05ba3b8e9e065cc81783fe82204
Leydson Vieira <leydson.vieira@gmail.com>
2019-03-22 10:03:16 -0300

    oba

--- mountbackup.py
+++ mountbackup.py
@@ -11,12 +11,10 @@ logging.basicConfig(filename='/var/log/mountbackup.log',level=logging.DEBUG)
 os.chdir('/')
 
 if os.path.exists("/bkp/dados"):
-
   os.system('umount /bkp')
   logging.info(' %s/%s/%s - %s:%s:%s backup desmontado' % (now.day, now.month, now.year, now.hour, now.minute, now.second))
 
 else:
-
   os.system('mount -a')
   logging.info(' %s/%s/%s - %s:%s:%s backup montado' % (now.day, now.month, now.year, now.hour, now.minute, now.second))
 
